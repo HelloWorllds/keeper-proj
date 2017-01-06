e2c22f1a6cfa190aa84d0ec1a3e2b3b6290a439b
HelloWorllds <dark____angel@mail.ru>
2017-01-06 13:02:36 +0200

    start proj

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBaK3yPJP9p3F3tVC5TRVZntd08DDpiPhQ",
+	authDomain: "keeper-4e597.firebaseapp.com",
+	databaseURL: "https://keeper-4e597.firebaseio.com",
+	storageBucket: "keeper-4e597.appspot.com",
+	messagingSenderId: "874065713541"
+};
+
+firebase.initializeApp(config);
+
+$(document).ready(function() {
+	
+});
